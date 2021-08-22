a22fd34bbd606d9109e0735b6b53a006e9c143b8
kalevishal <kalevishal1610@gmail.com>
2021-08-22 17:29:59 +0530

    Added: Die to get number between 1 to 6

---
+++ src/DieRolls.java
@@ -0,0 +1,11 @@
+public class DieRolls {
+    
+
+        public static void main(String[] args) {
+            int position=0;
+            int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Die Rolled:"+dieRoll);
+        }
+
+
+}
