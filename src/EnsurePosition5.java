2ce07d7135891a4c8afffb7624227715dd348d55
kalevishal <kalevishal1610@gmail.com>
2021-08-22 17:45:43 +0530

    Added: gets to exact winning position 100

---
+++ src/EnsurePosition5.java
@@ -0,0 +1,38 @@
+public class EnsurePosition5 {
+
+    public static void main(String[] args) {
+        int position=0;
+        while (position != 100) {
+            int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Die Rolled:"+dieRoll);
+
+            int checkOption=(int)Math.floor(Math.random()*10)%3;
+            if (checkOption == 0) {
+                System.out.println("No Play");
+            }
+            else if (checkOption == 1) {
+                System.out.println("Ladder Comes");
+                if(position+dieRoll<=100)
+                {
+                    position+=dieRoll;
+                }
+
+            }
+            else {
+                System.out.println("Snake Comes");
+                if((position-dieRoll)<0)
+                {
+                    position=0;
+                }
+                else
+                {
+                    position-=dieRoll;
+                }
+            }
+        }
+
+    }
+
+
+
+}
